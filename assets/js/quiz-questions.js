4f3630afd5e650a6dcf493d7a1b6dcda4b2f9e0a
mh <maxhalverson@live.com>
2023-05-24 17:22:03 -0500

    added some style to landing page. Started adding questions to js quiz-questions

---
+++ assets/js/quiz-questions.js
@@ -0,0 +1,26 @@
+var questions = [
+
+    {
+        question: "Commonly used data types do NOT include:",
+        choices: ['numbers', 'strings', 'booleans', 'alerts'],
+        answer: 'alerts'
+
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within _____.",
+        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses'
+
+    },
+    {
+        question: "Which is NOT a JS data type?",
+        choices: ['pickles', 'strings,' 'booleans', 'integers'],
+        answer: 'pickles'
+
+    },
+
+]
+
+questions[currentQuestion].choices
+questions[currentQuestion].question
+questions[currentQuestion].answer
