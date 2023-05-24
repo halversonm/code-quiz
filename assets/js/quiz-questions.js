aa9a350e00e82c78e8afd0f373fc8ccbc87cc177
mh <maxhalverson@live.com>
2023-05-24 17:40:42 -0500

    finished adding in all questions to quiz-questions

--- assets/js/quiz-questions.js
+++ assets/js/quiz-questions.js
@@ -1,26 +1,33 @@
 var questions = [
-
     {
-        question: "Commonly used data types do NOT include:",
+        question: "Commonly used data types does NOT include:",
         choices: ['numbers', 'strings', 'booleans', 'alerts'],
         answer: 'alerts'
-
     },
     {
         question: "The condition in an if/else statement is enclosed within _____.",
         choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
         answer: 'parentheses'
-
     },
     {
-        question: "Which is NOT a JS data type?",
-        choices: ['pickles', 'strings,' 'booleans', 'integers'],
-        answer: 'pickles'
-
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above'
     },
-
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'quotes'
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+        answer: 'console.log'
+    },
 ]
 
-questions[currentQuestion].choices
-questions[currentQuestion].question
-questions[currentQuestion].answer
+console.log(questions[0]);
+console.log(questions[0].choices[1]);
+// questions[currentQuestion].choices
+// questions[currentQuestion].question
+// questions[currentQuestion].answer
